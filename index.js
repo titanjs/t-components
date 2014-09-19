a2d2916ec97c226a67c9392d2c46304fb1353cb1
Kyle Finley <git@kylefinley.com>
2014-09-19 14:33:55 -0500

    Added new packages

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 module.exports = function(app, options) {
-  app.component();
-  app.loadStyles();
+  app.component(require('./t-icon'));
+  app.component(require('./t-calendar'));
+  app.component(require('./t-checkbox'));
+  app.component(require('./t-datetime'));
+  app.component(require('./t-datetime-input'));
+  app.component(require('./t-dropdown'));
+  app.component(require('./t-dialog'));
+  app.component(require('./t-favicons'));
+  app.component(require('./t-image'));
+  app.component(require('./t-input'));
+  app.component(require('./t-radio-button'));
+  app.component(require('./t-star-rating'));
+  app.component(require('./t-stared'));
+  // app.loadStyles();
 };
