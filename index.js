afac37a87c75895e106ab95d85f89b74cb51e761
Kyle Finley <git@kylefinley.com>
2014-09-24 15:58:04 -0500

    FIX: styles

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ module.exports = function(app, options) {
   app.component(require('./t-checkbox'));
   app.component(require('./t-datetime'));
   app.component(require('./t-datetime-input'));
+  app.component(require('./t-drawer-panel'));
   app.component(require('./t-dropdown'));
   app.component(require('./t-dialog'));
   app.component(require('./t-favicons'));
+  app.component(require('./t-header-panel'));
   app.component(require('./t-image'));
   app.component(require('./t-input'));
   app.component(require('./t-radio-button'));
+  app.component(require('./t-radio-group'));
   app.component(require('./t-star-rating'));
   app.component(require('./t-stared'));
   // app.loadStyles();
