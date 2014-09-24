0e5d6a4895bc76b4d266d90e7874ae51779a927d
Kyle Finley <git@kylefinley.com>
2014-09-24 15:59:12 -0500

    FIX: radio-group

--- t-radio-group/index.js
+++ t-radio-group/index.js
@@ -1,3 +1,22 @@
 module.exports = RadioGroup;
 function RadioGroup() {}
 RadioGroup.prototype.view = __dirname;
+
+RadioGroup.prototype.create = function(model, dom) {
+  // Watch the change of the options and update the selected element if needed
+  model.on('change', 'options', function() {
+    model.set('value', model.get('value'));
+  });
+};
+
+RadioGroup.prototype.select = function(option) {
+  this.model.set('value', optionValue(option));
+};
+
+RadioGroup.prototype.isSelected = function(option) {
+  return option.value === this.model.get('value') ? true : false;
+};
+
+function optionValue(option) {
+  return (option.hasOwnProperty('value')) ? option.value : option.content;
+}
